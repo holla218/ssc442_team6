36498fb065b80779d0c0908e7a266731446a9280
Maura Glynn <mauraglynn@Mauras-MacBook-Pro-6.local>
2020-01-18 18:54:13 -0500

    Working on 1b

--- lab1 - part 1.R
+++ lab1 - part 1.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(gapminder)
+library(ggplot2)
 data = mpg
 
 p1 <- ggplot(data) 
@@ -9,3 +11,23 @@ p2 + geom_point(mapping = aes(x=class,y=drv),size = 2, color = "navy")
 
 p1_2 <- ggplot(data) 
 p1_2 + geom_point(mapping = aes(x=displ,y=hwy,color=class))
+
+p1
+p2
+p1_2
+
+data2 = gapminder
+p1b <- ggplot(data2)
+p1b + geom_point(mapping = aes(x = gdpPercap, y = lifeExp))
+p1b
+p1b_2 <- p1b + geom_smooth(mapping = aes(x = gdpPercap, y=lifeExp))
+p1b_2
+p1b_3 <- p1b + geom_point(mapping = aes(x = gdpPercap, y = lifeExp)) + geom_smooth(mapping = aes(x = gdpPercap, y=lifeExp), method = "lm") + scale_x_log10()
+p1b_3
+
+
+library(scales)
+p4b <- p1b + geom_point(mapping = aes(x = gdpPercap, y = lifeExp)) +
+  geom_smooth((mapping = aes(x = gdpPercap, y = lifeExp)), method = "lm") +
+  scale_x_log10(labels = scales::dollar)
+p4b
