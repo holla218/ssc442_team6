9cbc74361f765e0a2e2bc8239c547940f70db704
Alex <holla218@msu.edu>
2020-02-10 20:47:42 -0500

    part 1 done

--- lab3/lab 3 .R
+++ lab3/lab 3 .R
@@ -28,47 +28,47 @@ summary(fit_start)
 step(fit_start, direction = "forward", scope=formula(fit_all))
 #this will produce an ordered list of the next variable with the lowest RSS
 
-fit_2 = lm(SalePrice ~ x1, data=Ames) #where x1 is the variable with the lowest RSS
+fit_2 = lm(SalePrice ~ ExterQual, data=Ames) #where x1 is the variable with the lowest RSS
 step(fit_2, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_3= lm(SalePrice ~ x1 + x2, data=Ames) 
+fit_3= lm(SalePrice ~ ExterQual + GrLivArea, data=Ames) 
 step(fit_3, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_4= lm(SalePrice ~ x1 + x2 + x3, data=Ames) 
+fit_4= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood, data=Ames) 
 step(fit_4, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_5= lm(SalePrice ~ x1 + x2 + x3 + x4, data=Ames) 
+fit_5= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl, data=Ames) 
 step(fit_5, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_6= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5, data=Ames) 
+fit_6= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1, data=Ames) 
 step(fit_6, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_7= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6, data=Ames) 
+fit_7= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2, data=Ames) 
 step(fit_7, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_8= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7, data=Ames) 
+fit_8= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF, data=Ames) 
 step(fit_8, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_9= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8, data=Ames) 
+fit_9= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual, data=Ames) 
 step(fit_9, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_10= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9, data=Ames) 
+fit_10= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure, data=Ames) 
 step(fit_10, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_11= lm(SalePrice ~  x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10, data=Ames) 
+fit_11= lm(SalePrice ~  ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual, data=Ames) 
 step(fit_11, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_12= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11, data=Ames) 
+fit_12= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual + SaleCondition, data=Ames) 
 step(fit_12, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_13= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12, data=Ames) 
+fit_13= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual + SaleCondition + LotFrontage, data=Ames) 
 step(fit_13, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_14= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13, data=Ames) 
+fit_14= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual + SaleCondition + LotFrontage + LotConfig, data=Ames) 
 step(fit_14, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
-fit_15= lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14, data=Ames) 
+fit_15= lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual + SaleCondition + LotFrontage + LotConfig + CentralAir, data=Ames) 
 step(fit_15, direction = "forward", scope=formula(fit_all)) #finds the next variable with the lowest RSS
 
-final_fit = lm(SalePrice ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15, data = Ames)
+final_fit = lm(SalePrice ~ ExterQual + GrLivArea + Neighborhood + RoofMatl + BsmtFinSF1 + Condition2 + TotalBsmtSF + BsmtQual + BsmtExposure + KitchenQual+ SaleCondition + LotFrontage + LotConfig + CentralAir + GarageCars, data = Ames)
 
 
 fits <- list(fit_start, fit_2, fit_3, fit_4, fit_5, fit_6, fit_7, fit_8, fit_9, fit_10, fit_11, fit_12, fit_13, fit_14, fit_15, final_fit)
 RMSE <- list()
-complexity <- list(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+complexity <- list(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 
 for(i in fits){
   rss <- c(crossprod(i$residuals))
   mse <- rss / length(i$residuals)
   rmse <- sqrt(mse)
-  RMSE <- rmse
+  RMSE <- append(RMSE,rmse)
 }
 
 plot(complexity,RMSE,main = "Complexity progression", xlab = "Complexity",ylab = "RMSE")
