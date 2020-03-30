bcaf7eea3bf2c87271c4c92745b2dd40d4a34b20
Maura Glynn <mauraglynn@Mauras-MacBook-Pro-6.local>
2020-03-30 09:26:21 -0400

    Exercise 2 finished

--- Lab 4 /Lab4_Exercise2_Maura.R
+++ Lab 4 /Lab4_Exercise2_Maura.R
@@ -1,11 +1,17 @@
 ### Exercise 2
 library(tidyverse)
+library(kernlab)
+library(boot)
+
+
+make_conf_mat = function(predicted, actual) {
+  table(predicted = predicted, actual = actual)
+}
 
 bank <- read_csv("https://msudataanalytics.github.io/SSC442/Labs/data/bank.csv")
 
 set.seed(123)
-bank$y <- str_replace_all(bank$y, c('no'= '0', 'yes'='1'))
-bank$Y <- as.numeric(bank$y)
+bank$y = factor(bank$y)
 
 smp_size <- floor(0.8 * nrow(bank))
 train_ind <- sample(seq_len(nrow(bank)), size = smp_size)
@@ -13,14 +19,14 @@ train_ind <- sample(seq_len(nrow(bank)), size = smp_size)
 bank_trn <- bank[train_ind, ]
 bank_tst <- bank[-train_ind, ]
 
-fit_education = glm(Y ~ education,
+fit_education = glm(y ~ education,
                     data = bank_trn, family = binomial)
-fit_selected = glm(Y ~ education + balance + housing + month,
+fit_selected = glm(y ~ education + balance + housing + month,
                    data = bank_trn, family = binomial)
-fit_additive = glm(Y ~ .,
+fit_additive = glm(y ~ .,
                    data = bank_trn, family = binomial)
 
-fit_over = glm(Y ~ education * (.),
+fit_over = glm(y ~ education * (.),
                data = bank_trn, family = binomial, maxit = 50)
 cv.glm(bank_trn, fit_education, K = 10)$delta[1]
 cv.glm(bank_trn, fit_selected, K = 10)$delta[1]
@@ -28,9 +34,9 @@ cv.glm(bank_trn, fit_additive, K = 10)$delta[1]
 cv.glm(bank_trn, fit_over, K = 10)$delta[1]
 
 
-bank_tst_pred = ifelse(predict(fit_additive, bank_tst) > 0,1,0)
-bank_tst_pred = ifelse(predict(fit_additive, bank_tst, type = "response") > 0.5,1,0)
+bank_tst_pred = ifelse(predict(fit_additive, bank_tst) > 0,"yes","no")
+bank_tst_pred = ifelse(predict(fit_additive, bank_tst, type = "response") > 0.5,"yes","no")
+(conf_mat_50 = make_conf_mat(predicted = bank_tst_pred, actual = bank_tst$y))
 
-(conf_mat_50 = make_conf_mat(predicted = bank_tst_pred, actual = bank_tst$Y))
+table(bank_tst$y) / nrow(bank_tst)
 
-table(bank_tst$Y) / nrow(bank_tst)
